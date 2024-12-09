76568409837c64b60a269ad2099a2e81ecfea52a
Dreas21 <96311691+Dreas21@users.noreply.github.com>
2024-12-09 17:56:11 +0100

    Main

---
+++ ESERCIZIO1/Esercizio_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+List<int> listanumeri = new List<int>();
+
+Boolean continua = true;
+while(continua == true){
+    Console.Write("Inserisci il numero: ");
+    string input_num = Console.ReadLine();
+    int num;
+    if(int.TryParse(input_num, out num)){
+        if (num % 2 == 0){
+            Console.WriteLine($"Il {num} che hai fornito è pari");
+            listanumeri.Add(num);
+        } 
+        else{
+            Console.WriteLine($"Il {num} che hai fornito è dispari");
+            listanumeri.Add(num);
+        }
+    }
+    else{
+    Console.WriteLine($"{input_num} non è valido");
+    }
+    Console.WriteLine("Vuoi continuare ad inserire numeri?");
+    Console.WriteLine("[1] --> si");
+    Console.WriteLine("[2] --> no \n");
+    string nuovonum = Console.ReadLine();
+    int nm;
+    int.TryParse(nuovonum, out nm);
+    if(nm == 2){
+        continua = false;
+    }
+}
+Console.WriteLine("");
+Console.WriteLine("Che numeri vuoi visualizzare?");
+Console.WriteLine("[1] --> pari");
+Console.WriteLine("[2] --> dispari");
+string scelta = Console.ReadLine();
+int s;
+int.TryParse(scelta, out s);
+Console.WriteLine("");
+for (int i=0; i<listanumeri.Count(); i++){
+    if(s==1){
+        if(listanumeri[i]%2 == 0){
+            Console.WriteLine($"{listanumeri[i]}");
+        }
+    }
+    if(s==2){
+        if(listanumeri[i]%2 != 0){
+            Console.WriteLine($"{listanumeri[i]}");
+        }
+    }
+}
---
+++ ESERCIZIO2/Esercizio_2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Giocattolo{
+    public string Nome{get; private set;}
+    public string Materiale{get; private set;}
+    public decimal CostoProduzione{get; private set;}
+    public decimal PrezzoVendita{get; private set;}
+
+    public Giocattolo(string nome, string materiale, decimal costoproduzione, decimal prezzovendita){
+        Nome = nome;
+        Materiale = materiale;
+        CostoProduzione = costoproduzione;
+        PrezzoVendita = prezzovendita;
+    }
+}
+
+public class Fabbrica{
+    private List<Giocattolo> listag = new List<Giocattolo>();
+
+    public void addGiocattolo(Giocattolo g){
+        listag.Add(g);
+    }
+
+    public void printGiocattolo(){
+        for(int i=0; i<listag.Count(); i++){
+            Console.WriteLine($"Nome: {listag[i].Nome}, \nMateriale: {listag[i].Materiale}, \nPrezzo Vendita: {listag[i].PrezzoVendita}");
+        }
+    }
+
+    public decimal calcoloTotale(){
+        decimal somma = 0;
+        for(int i=0; i<listag.Count(); i++){
+            decimal margine = listag[i].PrezzoVendita - listag[i].CostoProduzione;
+            somma = somma + margine;
+        }
+        return somma;
+    }
+
+    public class Program{
+        public static void Main(){
+            Fabbrica Hamleys = new Fabbrica();
+            Hamleys.addGiocattolo(new Giocattolo("Teddy Bear", "Peluche", 3.00m, 7.50m));
+            Hamleys.addGiocattolo(new Giocattolo("Puzzle", "Cartone", 1.50m, 4.00m));
+            Console.WriteLine("Elenco Giocattoli:");
+            Hamleys.printGiocattolo();
+            Console.WriteLine();
+            Console.WriteLine($"Guadagno Totale: {Hamleys.calcoloTotale}");
+        }
+    }
+}
